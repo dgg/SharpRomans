9fd859e09d575c9d091029adf71924635e3451c2
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2014-05-12 21:09:44 +0200

    RomanFigure.Parse(char)

---
+++ src/SharpRomans.Tests/RomanFigure.spec.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using NUnit.Framework;
+using StoryQ;
+
+namespace SharpRomans.Tests
+{
+	[TestFixture]
+	public class RomanFigureSpec
+	{
+		[Test]
+		public void ParseChar()
+		{
+			new Story("parse a char")
+				.InOrderTo("get a figure instance")
+				.AsA("library user")
+				.IWant("to be able to parse a char")
+
+				.WithScenario("parse a defined figure")
+					.Given(aCharacter_, 'I')
+					.When(theCharIsParsed)
+					.Then(theFigure_Is, RomanFigure.I)
+
+				.WithScenario("parse an undefined figure")
+					.Given(aCharacter_, 'W')
+					.When(theCharIsParsed)
+					.Then(throwsArgumentException)
+
+				.WithScenario("figures are unique")
+					.Given(aCharacter_, 'X')
+					.When(theCharIsParsedAgain_, 'X')
+					.Then(isTheSameFigure)
+
+				.ExecuteWithReport();
+		}
+
+		char _character;
+		private void aCharacter_(char character)
+		{
+			_character = character;
+		}
+
+		Func<RomanFigure> _figure;
+		private void theCharIsParsed()
+		{
+			_figure = () => RomanFigure.Parse(_character);
+		}
+
+		private void theFigure_Is(RomanFigure figure)
+		{
+			Assert.That(_figure(), Is.EqualTo(figure));
+		}
+
+		private void throwsArgumentException()
+		{
+			TestDelegate cast = () => _figure();
+			Assert.That(cast, Throws.ArgumentException);
+		}
+
+		private RomanFigure _anotherFigure;
+		private void theCharIsParsedAgain_(char c)
+		{
+			_anotherFigure = RomanFigure.Parse(c);
+		}
+
+		private void isTheSameFigure()
+		{
+			Assert.That(_figure(), Is.SameAs(_anotherFigure));
+		}
+	}
+}
---
+++ src/SharpRomans/RomanFigure.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace SharpRomans
+{
+	public class RomanFigure
+	{
+		public char Figure { get; private set; }
+		public short Value { get; private set; }
+		public string Description { get; private set; }
+
+		public bool IsSubstractive { get; private set; }
+		public bool IsSingle { get; private set; }
+		public bool IsRepeteable { get; private set; }
+
+		private readonly string _string;
+		private RomanFigure(char figure, short value, string description, bool isSubstractive, bool isSingle, bool isRepeteable)
+		{
+			Figure = figure;
+			_string = figure.ToString(CultureInfo.InvariantCulture);
+			Value = value;
+			Description = description;
+			IsSubstractive = isSubstractive;
+			IsSingle = isSingle;
+			IsRepeteable = isRepeteable;
+		}
+
+		public static readonly RomanFigure N = new RomanFigure('N', 0, "nulla", false, false, false);
+		public static readonly RomanFigure I = new RomanFigure('I', 1, "unus", true, false, true);
+		public static readonly RomanFigure V = new RomanFigure('V', 5, "quique", false, true, false);
+		public static readonly RomanFigure X = new RomanFigure('X', 10, "decem", true, false, true);
+		public static readonly RomanFigure L = new RomanFigure('L', 50, "quinquaginta", false, true, false);
+		public static readonly RomanFigure C = new RomanFigure('C', 100, "centum", true, false, true);
+		public static readonly RomanFigure D = new RomanFigure('D', 500, "quingenti", false, true, false);
+		public static readonly RomanFigure M = new RomanFigure('M', 1000, "mille", false, false, true);
+
+		public static readonly IEnumerable<RomanFigure> All = new[] { N, I, V, X, L, C, D, M };
+
+		public override string ToString()
+		{
+			return _string;
+		}
+
+		public static RomanFigure Parse(char figure)
+		{
+			RomanFigure parsed;
+			if (!TryParse(figure, out parsed))
+			{
+				throw new ArgumentException(string.Format("Requested value '{0}' was not found", figure), "figure");
+			}
+			return parsed;
+		}
+
+		public static bool TryParse(char figure, out RomanFigure parsed)
+		{
+			parsed = All.FirstOrDefault(f => f.Figure.Equals(figure));
+			return parsed != null;
+		}
+		
+	}
+}
