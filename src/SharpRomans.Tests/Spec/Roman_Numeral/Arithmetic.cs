93b7a4d06f23e1deecb5eb051cd52bebe4292f18
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2017-05-05 11:53:02 +0200

    fixed feature RomanNumeral.Arithmetic

--- src/SharpRomans.Tests/Spec/Roman_Numeral/Arithmetic.cs
+++ src/SharpRomans.Tests/Spec/Roman_Numeral/Arithmetic.cs
@@ -6,7 +6,7 @@ using Xunit;
 
 namespace SharpRomans.Tests.Spec.Roman_Numeral
 {
-	[Category("Spec"), Category("RomanNumeral"), Category("Arithmetic")]
+	[Category("Spec", Subject = "RomanNumeral", Feature = "Arithmetic")]
 	[Collection("bddfy")]
 	[Story(
 		Title = "try parse a string",
@@ -20,22 +20,22 @@ namespace SharpRomans.Tests.Spec.Roman_Numeral
 		public void Addition()
 		{
 			this.WithTags("RomanNumeral", "Arithmetic", "Addition", "plus")
-				.Given(_ => _.theRomanNumeral_(new RomanNumeral(20)))
-				.When(_ => _.plus_(new RomanNumeral(11)))
-				.Then(_ => _.theResultIs_(new RomanNumeral(31)))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(20)))
+				.When(_ => _.plus(new RomanNumeral(11)))
+				.Then(_ => _.theResultIs(new RomanNumeral(31)))
 				.And(_ => _.isNotDestructive())
 				.BDDfy("bounded operation");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Addition", "plus")
-				.Given(_ => _.theRomanNumeral_(RomanNumeral.Max))
-				.When(_ => _.plus_(new RomanNumeral(1)))
+				.Given(_ => _.theRomanNumeral(RomanNumeral.Max))
+				.When(_ => _.plus(new RomanNumeral(1)))
 				.Then(_ => _.theResultOverflows())
 				.BDDfy("overflowing operation");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Addition", "plus")
-				.Given(_ => _.theRomanNumeral_(new RomanNumeral(13)))
-				.When(_ => _.plus_((RomanNumeral) null))
-				.Then(_ => _.theResultIsTheSameAs_(_subject))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(13)))
+				.When(_ => _.plus((RomanNumeral) null))
+				.Then(_ => _.theResultIsTheSameAs(_subject))
 				.BDDfy("adding NULL");
 		}
 
@@ -43,34 +43,34 @@ namespace SharpRomans.Tests.Spec.Roman_Numeral
 		public void AdditionOperation()
 		{
 			this.WithTags("RomanNumeral", "Arithmetic", "Addition", "add")
-				.Given(_ => _.theRomanNumeral_(new RomanNumeral(20)))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(20)))
 				.When(_ => _.added_(new RomanNumeral(11)))
-				.Then(_ => _.theResultIs_(new RomanNumeral(31)))
+				.Then(_ => _.theResultIs(new RomanNumeral(31)))
 				.And(_ => _.isNotDestructive())
 				.BDDfy("bounded operation");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Addition", "add")
-				.Given(_ => _.theRomanNumeral_(RomanNumeral.Max))
+				.Given(_ => _.theRomanNumeral(RomanNumeral.Max))
 				.When(_ => _.added_(new RomanNumeral(1)))
 				.Then(_ => _.theResultOverflows())
 				.BDDfy("overflowing operation");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Addition", "add")
-				.Given(_ => _.theRomanNumeral_(new RomanNumeral(13)))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(13)))
 				.When(_ => _.added_((RomanNumeral)null))
-				.Then(_ => _.theResultIsTheSameAs_(_subject))
+				.Then(_ => _.theResultIsTheSameAs(_subject))
 				.BDDfy("adding NULL to not NULL");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Addition", "add")
-				.Given(_ => _.theRomanNumeral_(null))
+				.Given(_ => _.theRomanNumeral(null))
 				.When(_ => _.added_(new RomanNumeral(13)))
-				.Then(_ => _.theResultIsTheSameAs_(_operand))
+				.Then(_ => _.theResultIsTheSameAs(_operand))
 				.BDDfy("adding not NULL to NULL");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Addition", "add")
-				.Given(_ => _.theRomanNumeral_(null))
+				.Given(_ => _.theRomanNumeral(null))
 				.When(_ => _.added_((RomanNumeral)null))
-				.Then(_ => _.theResultIs_(null))
+				.Then(_ => _.theResultIs(null))
 				.BDDfy("adding NULL to NULL");
 		}
 
@@ -78,22 +78,22 @@ namespace SharpRomans.Tests.Spec.Roman_Numeral
 		public void RomanFigureAddition()
 		{
 			this.WithTags("RomanNumeral", "Arithmetic", "Addition", "plus figure")
-				.Given(_ => _.theRomanNumeral_(new RomanNumeral(20)))
-				.When(_ => _.plus_(RomanFigure.V))
-				.Then(_ => _.theResultIs_(new RomanNumeral(25)))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(20)))
+				.When(_ => _.plus(RomanFigure.V))
+				.Then(_ => _.theResultIs(new RomanNumeral(25)))
 				.And(_ => _.isNotDestructive())
 				.BDDfy("bounded operation");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Addition", "plus figure")
-				.Given(_ => _.theRomanNumeral_(RomanNumeral.Max))
-				.When(_ => _.plus_(RomanFigure.I))
+				.Given(_ => _.theRomanNumeral(RomanNumeral.Max))
+				.When(_ => _.plus(RomanFigure.I))
 				.Then(_ => _.theResultOverflows())
 				.BDDfy("overflowing operation");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Addition", "plus figure")
-				.Given(_ => _.theRomanNumeral_(new RomanNumeral(13)))
-				.When(_ => _.plus_((RomanFigure)null))
-				.Then(_ => _.theResultIsTheSameAs_(_subject))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(13)))
+				.When(_ => _.plus((RomanFigure)null))
+				.Then(_ => _.theResultIsTheSameAs(_subject))
 				.BDDfy("adding NULL");
 		}
 
@@ -101,34 +101,34 @@ namespace SharpRomans.Tests.Spec.Roman_Numeral
 		public void RomanFigureAdditionOperation()
 		{
 			this.WithTags("RomanNumeral", "Arithmetic", "Addition", "add figure")
-				.Given(_ => _.theRomanNumeral_(new RomanNumeral(20)))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(20)))
 				.When(_ => _.added_(RomanFigure.V))
-				.Then(_ => _.theResultIs_(new RomanNumeral(25)))
+				.Then(_ => _.theResultIs(new RomanNumeral(25)))
 				.And(_ => _.isNotDestructive())
 				.BDDfy("bounded operation");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Addition", "add figure")
-				.Given(_ => _.theRomanNumeral_(RomanNumeral.Max))
+				.Given(_ => _.theRomanNumeral(RomanNumeral.Max))
 				.When(_ => _.added_(RomanFigure.I))
 				.Then(_ => _.theResultOverflows())
 				.BDDfy("overflowing operation");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Addition", "add figure")
-				.Given(_ => _.theRomanNumeral_(new RomanNumeral(13)))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(13)))
 				.When(_ => _.added_((RomanFigure)null))
-				.Then(_ => _.theResultIsTheSameAs_(_subject))
+				.Then(_ => _.theResultIsTheSameAs(_subject))
 				.BDDfy("adding NULL to not NULL");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Addition", "add figure")
-				.Given(_ => _.theRomanNumeral_(null))
+				.Given(_ => _.theRomanNumeral(null))
 				.When(_ => _.added_(RomanFigure.V))
-				.Then(_ => _.theResultIs_(new RomanNumeral(5)))
+				.Then(_ => _.theResultIs(new RomanNumeral(5)))
 				.BDDfy("adding not NULL to NULL");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Addition", "add figure")
-				.Given(_ => _.theRomanNumeral_(null))
+				.Given(_ => _.theRomanNumeral(null))
 				.When(_ => _.added_((RomanFigure)null))
-				.Then(_ => _.theResultIs_(null))
+				.Then(_ => _.theResultIs(null))
 				.BDDfy("adding NULL to NULL");
 		}
 
@@ -136,22 +136,22 @@ namespace SharpRomans.Tests.Spec.Roman_Numeral
 		public void Substraction()
 		{
 			this.WithTags("RomanNumeral", "Arithmetic", "Substraction", "minus")
-				.Given(_ => _.theRomanNumeral_(new RomanNumeral(20)))
-				.When(_ => _.minus_(new RomanNumeral(11)))
-				.Then(_ => _.theResultIs_(new RomanNumeral(9)))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(20)))
+				.When(_ => _.minus(new RomanNumeral(11)))
+				.Then(_ => _.theResultIs(new RomanNumeral(9)))
 				.And(_ => _.isNotDestructive())
 				.BDDfy("bounded operation");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Substraction", "minus")
-				.Given(_ => _.theRomanNumeral_(RomanNumeral.Min))
-				.When(_ => _.minus_(new RomanNumeral(1)))
+				.Given(_ => _.theRomanNumeral(RomanNumeral.Min))
+				.When(_ => _.minus(new RomanNumeral(1)))
 				.Then(_ => _.theResultOverflows())
 				.BDDfy("overflowing operation");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Substraction", "minus")
-				.Given(_ => _.theRomanNumeral_(new RomanNumeral(13)))
-				.When(_ => _.minus_((RomanNumeral)null))
-				.Then(_ => _.theResultIsTheSameAs_(_subject))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(13)))
+				.When(_ => _.minus((RomanNumeral)null))
+				.Then(_ => _.theResultIsTheSameAs(_subject))
 				.BDDfy("substracting NULL");
 		}
 
@@ -159,34 +159,34 @@ namespace SharpRomans.Tests.Spec.Roman_Numeral
 		public void SubstractionOperation()
 		{
 			this.WithTags("RomanNumeral", "Arithmetic", "Substraction", "substract")
-				.Given(_ => _.theRomanNumeral_(new RomanNumeral(20)))
-				.When(_ => _.substracted_(new RomanNumeral(11)))
-				.Then(_ => _.theResultIs_(new RomanNumeral(9)))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(20)))
+				.When(_ => _.substracted(new RomanNumeral(11)))
+				.Then(_ => _.theResultIs(new RomanNumeral(9)))
 				.And(_ => _.isNotDestructive())
 				.BDDfy("bounded operation");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Substraction", "substract")
-				.Given(_ => _.theRomanNumeral_(RomanNumeral.Min))
-				.When(_ => _.substracted_(new RomanNumeral(1)))
+				.Given(_ => _.theRomanNumeral(RomanNumeral.Min))
+				.When(_ => _.substracted(new RomanNumeral(1)))
 				.Then(_ => _.theResultOverflows())
 				.BDDfy("overflowing operation");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Substraction", "substract")
-				.Given(_ => _.theRomanNumeral_(new RomanNumeral(13)))
-				.When(_ => _.substracted_((RomanNumeral)null))
-				.Then(_ => _.theResultIsTheSameAs_(_subject))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(13)))
+				.When(_ => _.substracted((RomanNumeral)null))
+				.Then(_ => _.theResultIsTheSameAs(_subject))
 				.BDDfy("substracting NULL to not NULL");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Substraction", "substract")
-				.Given(_ => _.theRomanNumeral_(null))
-				.When(_ => _.substracted_(new RomanNumeral(13)))
-				.Then(_ => _.theResultIsTheSameAs_(_operand))
+				.Given(_ => _.theRomanNumeral(null))
+				.When(_ => _.substracted(new RomanNumeral(13)))
+				.Then(_ => _.theResultIsTheSameAs(_operand))
 				.BDDfy("substracting not NULL to NULL");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Substraction", "substract")
-				.Given(_ => _.theRomanNumeral_(null))
-				.When(_ => _.substracted_((RomanNumeral)null))
-				.Then(_ => _.theResultIs_(null))
+				.Given(_ => _.theRomanNumeral(null))
+				.When(_ => _.substracted((RomanNumeral)null))
+				.Then(_ => _.theResultIs(null))
 				.BDDfy("substracting NULL to NULL");
 		}
 
@@ -194,22 +194,22 @@ namespace SharpRomans.Tests.Spec.Roman_Numeral
 		public void RomanFigureSubstraction()
 		{
 			this.WithTags("RomanNumeral", "Arithmetic", "Substraction", "minus figure")
-				.Given(_ => _.theRomanNumeral_(new RomanNumeral(20)))
-				.When(_ => _.minus_(RomanFigure.V))
-				.Then(_ => _.theResultIs_(new RomanNumeral(15)))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(20)))
+				.When(_ => _.minus(RomanFigure.V))
+				.Then(_ => _.theResultIs(new RomanNumeral(15)))
 				.And(_ => _.isNotDestructive())
 				.BDDfy("bounded operation");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Substraction", "minus figure")
-				.Given(_ => _.theRomanNumeral_(RomanNumeral.Min))
-				.When(_ => _.minus_(RomanFigure.I))
+				.Given(_ => _.theRomanNumeral(RomanNumeral.Min))
+				.When(_ => _.minus(RomanFigure.I))
 				.Then(_ => _.theResultOverflows())
 				.BDDfy("overflowing operation");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Substraction", "minus figure")
-				.Given(_ => _.theRomanNumeral_(new RomanNumeral(13)))
-				.When(_ => _.minus_((RomanFigure)null))
-				.Then(_ => _.theResultIsTheSameAs_(_subject))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(13)))
+				.When(_ => _.minus((RomanFigure)null))
+				.Then(_ => _.theResultIsTheSameAs(_subject))
 				.BDDfy("substracting NULL");
 		}
 
@@ -217,46 +217,46 @@ namespace SharpRomans.Tests.Spec.Roman_Numeral
 		public void RomanFigureSubstractionOperation()
 		{
 			this.WithTags("RomanNumeral", "Arithmetic", "Substraction", "substract figure")
-				.Given(_ => _.theRomanNumeral_(new RomanNumeral(20)))
-				.When(_ => _.substracted_(RomanFigure.V))
-				.Then(_ => _.theResultIs_(new RomanNumeral(15)))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(20)))
+				.When(_ => _.substracted(RomanFigure.V))
+				.Then(_ => _.theResultIs(new RomanNumeral(15)))
 				.And(_ => _.isNotDestructive())
 				.BDDfy("bounded operation");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Substraction", "substract figure")
-				.Given(_ => _.theRomanNumeral_(RomanNumeral.Min))
-				.When(_ => _.substracted_(RomanFigure.I))
+				.Given(_ => _.theRomanNumeral(RomanNumeral.Min))
+				.When(_ => _.substracted(RomanFigure.I))
 				.Then(_ => _.theResultOverflows())
 				.BDDfy("overflowing operation");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Substraction", "substract figure")
-				.Given(_ => _.theRomanNumeral_(new RomanNumeral(13)))
-				.When(_ => _.substracted_((RomanFigure)null))
-				.Then(_ => _.theResultIsTheSameAs_(_subject))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(13)))
+				.When(_ => _.substracted((RomanFigure)null))
+				.Then(_ => _.theResultIsTheSameAs(_subject))
 				.BDDfy("substracting NULL from not NULL");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Substraction", "substract figure")
-				.Given(_ => _.theRomanNumeral_(null))
-				.When(_ => _.substracted_(RomanFigure.V))
-				.Then(_ => _.theResultIs_(new RomanNumeral(5)))
+				.Given(_ => _.theRomanNumeral(null))
+				.When(_ => _.substracted(RomanFigure.V))
+				.Then(_ => _.theResultIs(new RomanNumeral(5)))
 				.BDDfy("substracting not NULL to NULL");
 
 			this.WithTags("RomanNumeral", "Arithmetic", "Substraction", "substract figure")
-				.Given(_ => _.theRomanNumeral_(null))
-				.When(_ => _.substracted_((RomanFigure)null))
-				.Then(_ => _.theResultIs_(null))
+				.Given(_ => _.theRomanNumeral(null))
+				.When(_ => _.substracted((RomanFigure)null))
+				.Then(_ => _.theResultIs(null))
 				.BDDfy("substracting NULL to NULL");
 		}
 
 		RomanNumeral _subject;
-		private void theRomanNumeral_(RomanNumeral subject)
+		private void theRomanNumeral(RomanNumeral subject)
 		{
 			_subject = subject;
 		}
 
 		private Func<RomanNumeral> _operation;
 		private IValuable _operand;
-		private void plus_(RomanNumeral operand)
+		private void plus(RomanNumeral operand)
 		{
 			_operand = operand;
 			_operation = () => _subject.Plus(operand);
@@ -268,7 +268,7 @@ namespace SharpRomans.Tests.Spec.Roman_Numeral
 			_operation = () => _subject + operand;
 		}
 
-		private void plus_(RomanFigure operand)
+		private void plus(RomanFigure operand)
 		{
 			_operand = operand;
 			_operation = () => _subject.Plus(operand);
@@ -280,32 +280,32 @@ namespace SharpRomans.Tests.Spec.Roman_Numeral
 			_operation = () => _subject + operand;
 		}
 
-		private void minus_(RomanNumeral operand)
+		private void minus(RomanNumeral operand)
 		{
 			_operand = operand;
 			_operation = () => _subject.Minus(operand);
 		}
 
-		private void substracted_(RomanNumeral operand)
+		private void substracted(RomanNumeral operand)
 		{
 			_operand = operand;
 			_operation = () => _subject - operand;
 		}
 
-		private void minus_(RomanFigure operand)
+		private void minus(RomanFigure operand)
 		{
 			_operand = operand;
 			_operation = () => _subject.Minus(operand);
 		}
 
-		private void substracted_(RomanFigure operand)
+		private void substracted(RomanFigure operand)
 		{
 			_operand = operand;
 			_operation = () => _subject - operand;
 		}
 
 		private RomanNumeral _result;
-		private void theResultIs_(RomanNumeral result)
+		private void theResultIs(RomanNumeral result)
 		{
 			_result = result;
 			Assert.Equal(result, _operation());
@@ -327,7 +327,7 @@ namespace SharpRomans.Tests.Spec.Roman_Numeral
 			Assert.Contains(RomanNumeral.MaxValue.ToString(CultureInfo.InvariantCulture), ex.Message);
 		}
 
-		private void theResultIsTheSameAs_(IValuable same)
+		private void theResultIsTheSameAs(IValuable same)
 		{
 			Assert.Same(same, _operation());
 		}
