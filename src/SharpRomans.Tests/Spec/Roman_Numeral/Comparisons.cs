1ac30bf7d907b6099631975f45fa5d8bbaf29e79
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2017-05-05 12:01:28 +0200

    fixed feature RomanNumeral.Comparisons

--- src/SharpRomans.Tests/Spec/Roman_Numeral/Comparisons.cs
+++ src/SharpRomans.Tests/Spec/Roman_Numeral/Comparisons.cs
@@ -5,7 +5,7 @@ using Xunit;
 
 namespace SharpRomans.Tests.Spec.Roman_Numeral
 {
-	[Category("Spec"), Category("RomanNumeral"), Category("Comparisons")]
+	[Category("Spec", Subject = "RomanNumeral", Feature = "Comparisons")]
 	[Collection("bddfy")]
 	[Story(
 		Title = "roman numeral comparison",
@@ -19,27 +19,27 @@ namespace SharpRomans.Tests.Spec.Roman_Numeral
 		public void CompareToRomanNumeral()
 		{
 			this.WithTags("RomanNumeral", "Comparisons", "Compare")
-				.Given(_ => _.TheRomanNumeral_(new RomanNumeral(5)))
-				.When(_ => _.ComparedTo_(new RomanNumeral(1)))
-				.Then(_ => _.IsGreaterThanZero())
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(5)))
+				.When(_ => _.comparedTo(new RomanNumeral(1)))
+				.Then(_ => _.isGreaterThanZero())
 				.BDDfy("one numeral is greater than another");
 
 			this.WithTags("RomanNumeral", "Comparisons", "Compare")
-				.Given(_ => _.TheRomanNumeral_(new RomanNumeral(1)))
-				.When(_ => _.ComparedTo_(new RomanNumeral(10)))
-				.Then(_ => _.IsLessThanZero())
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(1)))
+				.When(_ => _.comparedTo(new RomanNumeral(10)))
+				.Then(_ => _.isLessThanZero())
 				.BDDfy("one numeral is less than another");
 
 			this.WithTags("RomanNumeral", "Comparisons", "Compare")
-				.Given(_ => _.TheRomanNumeral_(new RomanNumeral(10)))
-				.When(_ => _.ComparedTo_(new RomanNumeral(10)))
-				.Then(_ => _.IsZero())
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(10)))
+				.When(_ => _.comparedTo(new RomanNumeral(10)))
+				.Then(_ => _.isZero())
 				.BDDfy("one numeral is equal to another");
 
 			this.WithTags("RomanNumeral", "Comparisons", "Compare")
-				.Given(_ => _.TheRomanNumeral_(new RomanNumeral(0)))
-				.When(_ => _.ComparedTo_((RomanNumeral)null))
-				.Then(_ => _.IsGreaterThanZero())
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(0)))
+				.When(_ => _.comparedTo(null))
+				.Then(_ => _.isGreaterThanZero())
 				.BDDfy("one figure is compared to NULL");
 		}
 
@@ -47,33 +47,33 @@ namespace SharpRomans.Tests.Spec.Roman_Numeral
 		public void GreaterThanRomanNumeral()
 		{
 			this.WithTags("RomanNumeral", "Comparisons", "greather than")
-				.Given(_ => _.TheRomanNumeral_(new RomanNumeral(5)))
-				.When(_ => _._IsEvaluatedAgainst_(Op.Gt, new RomanNumeral(1)))
-				.Then(_ => _.Is_(true))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(5)))
+				.When(_ => _._IsEvaluatedAgainst_(Op.Gt, new RomanNumeral(1)), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(true))
 				.BDDfy("one numeral is greater than another");
 
 			this.WithTags("RomanNumeral", "Comparisons", "greather than")
-				.Given(_ => _.TheRomanNumeral_(new RomanNumeral(0)))
-				.When(_ => _._IsEvaluatedAgainst_(Op.Gt, new RomanNumeral(1)))
-				.Then(_ => _.Is_(false))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(0)))
+				.When(_ => _._IsEvaluatedAgainst_(Op.Gt, new RomanNumeral(1)), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(false))
 				.BDDfy("one numeral is less than another");
 
 			this.WithTags("RomanNumeral", "Comparisons", "greather than")
-				.Given(_ => _.TheRomanNumeral_(new RomanNumeral(10)))
-				.When(_ => _._IsEvaluatedAgainst_(Op.Gt, new RomanNumeral(10)))
-				.Then(_ => _.Is_(false))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(10)))
+				.When(_ => _._IsEvaluatedAgainst_(Op.Gt, new RomanNumeral(10)), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(false))
 				.BDDfy("one numeral is equal to another");
 
 			this.WithTags("RomanNumeral", "Comparisons", "greather than")
-				.Given(_ => _.TheRomanNumeral_(RomanNumeral.Zero))
-				.When(_ => _._IsEvaluatedAgainst_(Op.Gt, (RomanNumeral)null))
-				.Then(_ => _.Is_(true))
+				.Given(_ => _.theRomanNumeral(RomanNumeral.Zero))
+				.When(_ => _._IsEvaluatedAgainst_(Op.Gt, null), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(true))
 				.BDDfy("one numeral is compared to NULL");
 
 			this.WithTags("RomanNumeral", "Comparisons", "greather than")
-				.Given(_ => _.TheRomanNumeral_((RomanNumeral)null))
-				.When(_ => _._IsEvaluatedAgainst_(Op.Gt, RomanNumeral.Zero))
-				.Then(_ => _.Is_(false))
+				.Given(_ => _.theRomanNumeral(null))
+				.When(_ => _._IsEvaluatedAgainst_(Op.Gt, RomanNumeral.Zero), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(false))
 				.BDDfy("NULL is compared to a numeral");
 		}
 
@@ -81,34 +81,34 @@ namespace SharpRomans.Tests.Spec.Roman_Numeral
 		public void GreaterThanOrEqualRomanNumeral()
 		{
 			this.WithTags("RomanNumeral", "Comparisons", "greather than or equal")
-				.Given(_ => _.TheRomanNumeral_(new RomanNumeral(5)))
-				.When(_ => _._IsEvaluatedAgainst_(Op.GtE, new RomanNumeral(1)))
-				.Then(_ => _.Is_(true))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(5)))
+				.When(_ => _._IsEvaluatedAgainst_(Op.GtE, new RomanNumeral(1)), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(true))
 				.BDDfy("one numeral is greater than another");
 
 			this.WithTags("RomanNumeral", "Comparisons", "greather than or equal")
-				.Given(_ => _.TheRomanNumeral_(new RomanNumeral(1)))
-				.When(_ => _._IsEvaluatedAgainst_(Op.GtE, new RomanNumeral(11)))
-				.Then(_ => _.Is_(false))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(1)))
+				.When(_ => _._IsEvaluatedAgainst_(Op.GtE, new RomanNumeral(11)), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(false))
 				.BDDfy("one numeral is less than another");
 
 			this.WithTags("RomanNumeral", "Comparisons", "greather than or equal")
-				.Given(_ => _.TheRomanNumeral_(new RomanNumeral(11)))
-				.When(_ => _._IsEvaluatedAgainst_(Op.GtE, new RomanNumeral(11)))
-				.Then(_ => _.Is_(true))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(11)))
+				.When(_ => _._IsEvaluatedAgainst_(Op.GtE, new RomanNumeral(11)), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(true))
 				.BDDfy("one numeral is equal to another");
 
 			this.WithTags("RomanNumeral", "Comparisons", "greather than or equal")
-				.Given(_ => _.TheRomanNumeral_(RomanNumeral.Zero))
-				.When(_ => _._IsEvaluatedAgainst_(Op.Gt, (RomanNumeral)null))
-				.Then(_ => _.Is_(true))
+				.Given(_ => _.theRomanNumeral(RomanNumeral.Zero))
+				.When(_ => _._IsEvaluatedAgainst_(Op.Gt, null), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(true))
 				.BDDfy("one numeral is compared to NULL");
 
 
 			this.WithTags("RomanNumeral", "Comparisons", "greather than or equal")
-				.Given(_ => _.TheRomanNumeral_((RomanNumeral)null))
-				.When(_ => _._IsEvaluatedAgainst_(Op.GtE, RomanNumeral.Zero))
-				.Then(_ => _.Is_(false))
+				.Given(_ => _.theRomanNumeral(null))
+				.When(_ => _._IsEvaluatedAgainst_(Op.GtE, RomanNumeral.Zero), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(false))
 				.BDDfy("NULL is compared to a numeral");
 		}
 
@@ -116,33 +116,33 @@ namespace SharpRomans.Tests.Spec.Roman_Numeral
 		public void LessThanRomanNumeral()
 		{
 			this.WithTags("RomanNumeral", "Comparisons", "less than")
-				.Given(_ => _.TheRomanNumeral_(new RomanNumeral(5)))
-				.When(_ => _._IsEvaluatedAgainst_(Op.Lt, new RomanNumeral(1)))
-				.Then(_ => _.Is_(false))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(5)))
+				.When(_ => _._IsEvaluatedAgainst_(Op.Lt, new RomanNumeral(1)), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(false))
 				.BDDfy("one numeral is greater than another");
 
 			this.WithTags("RomanNumeral", "Comparisons", "less than")
-				.Given(_ => _.TheRomanNumeral_(new RomanNumeral(1)))
-				.When(_ => _._IsEvaluatedAgainst_(Op.Lt, new RomanNumeral(10)))
-				.Then(_ => _.Is_(true))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(1)))
+				.When(_ => _._IsEvaluatedAgainst_(Op.Lt, new RomanNumeral(10)), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(true))
 				.BDDfy("one numeral is less than another");
 
 			this.WithTags("RomanNumeral", "Comparisons", "less than")
-				.Given(_ => _.TheRomanNumeral_(new RomanNumeral(10)))
-				.When(_ => _._IsEvaluatedAgainst_(Op.Lt, new RomanNumeral(10)))
-				.Then(_ => _.Is_(false))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(10)))
+				.When(_ => _._IsEvaluatedAgainst_(Op.Lt, new RomanNumeral(10)), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(false))
 				.BDDfy("one numeral is equal to another");
 
 			this.WithTags("RomanNumeral", "Comparisons", "less than")
-				.Given(_ => _.TheRomanNumeral_(RomanNumeral.Zero))
-				.When(_ => _._IsEvaluatedAgainst_(Op.Lt, (RomanNumeral)null))
-				.Then(_ => _.Is_(false))
+				.Given(_ => _.theRomanNumeral(RomanNumeral.Zero))
+				.When(_ => _._IsEvaluatedAgainst_(Op.Lt, null), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(false))
 				.BDDfy("one numeral is compared to NULL");
 
 			this.WithTags("RomanNumeral", "Comparisons", "less than")
-				.Given(_ => _.TheRomanNumeral_((RomanNumeral)null))
-				.When(_ => _._IsEvaluatedAgainst_(Op.Lt, RomanNumeral.Zero))
-				.Then(_ => _.Is_(true))
+				.Given(_ => _.theRomanNumeral(null))
+				.When(_ => _._IsEvaluatedAgainst_(Op.Lt, RomanNumeral.Zero), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(true))
 				.BDDfy("NULL is compared to a numeral");
 		}
 
@@ -150,44 +150,44 @@ namespace SharpRomans.Tests.Spec.Roman_Numeral
 		public void LessThanOrEqualToRomanNumeral()
 		{
 			this.WithTags("RomanNumeral", "Comparisons", "less than or equals")
-				.Given(_ => _.TheRomanNumeral_(new RomanNumeral(5)))
-				.When(_ => _._IsEvaluatedAgainst_(Op.LtE, new RomanNumeral(1)))
-				.Then(_ => _.Is_(false))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(5)))
+				.When(_ => _._IsEvaluatedAgainst_(Op.LtE, new RomanNumeral(1)), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(false))
 				.BDDfy("one numeral is greater than another");
 
 			this.WithTags("RomanNumeral", "Comparisons", "less than or equals")
-				.Given(_ => _.TheRomanNumeral_(new RomanNumeral(1)))
-				.When(_ => _._IsEvaluatedAgainst_(Op.LtE, new RomanNumeral(10)))
-				.Then(_ => _.Is_(true))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(1)))
+				.When(_ => _._IsEvaluatedAgainst_(Op.LtE, new RomanNumeral(10)), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(true))
 				.BDDfy("one numeral is less than another");
 
 			this.WithTags("RomanNumeral", "Comparisons", "less than or equals")
-				.Given(_ => _.TheRomanNumeral_(new RomanNumeral(10)))
-				.When(_ => _._IsEvaluatedAgainst_(Op.LtE, new RomanNumeral(10)))
-				.Then(_ => _.Is_(true))
+				.Given(_ => _.theRomanNumeral(new RomanNumeral(10)))
+				.When(_ => _._IsEvaluatedAgainst_(Op.LtE, new RomanNumeral(10)), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(true))
 				.BDDfy("one numeral is equal to another");
 
 			this.WithTags("RomanNumeral", "Comparisons", "less than or equals")
-			.Given(_ => _.TheRomanNumeral_(RomanNumeral.Zero))
-				.When(_ => _._IsEvaluatedAgainst_(Op.LtE, (RomanNumeral)null))
-				.Then(_ => _.Is_(false))
+			.Given(_ => _.theRomanNumeral(RomanNumeral.Zero))
+				.When(_ => _._IsEvaluatedAgainst_(Op.LtE, null), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(false))
 				.BDDfy("one numeral is compared to NULL");
 
 			this.WithTags("RomanNumeral", "Comparisons", "less than or equals")
-				.Given(_ => _.TheRomanNumeral_((RomanNumeral)null))
-				.When(_ => _._IsEvaluatedAgainst_(Op.LtE, RomanNumeral.Zero))
-				.Then(_ => _.Is_(true))
+				.Given(_ => _.theRomanNumeral(null))
+				.When(_ => _._IsEvaluatedAgainst_(Op.LtE, RomanNumeral.Zero), "'{0}' is evaluated against {1}")
+				.Then(_ => _.@is(true))
 				.BDDfy("NULL is compared to a numeral");
 		}
 
 		RomanNumeral _subject;
-		private void TheRomanNumeral_(RomanNumeral numeral)
+		private void theRomanNumeral(RomanNumeral numeral)
 		{
 			_subject = numeral;
 		}
 
 		private int _comparison;
-		private void ComparedTo_(RomanNumeral another)
+		private void comparedTo(RomanNumeral another)
 		{
 			_comparison = _subject.CompareTo(another);
 		}
@@ -198,22 +198,22 @@ namespace SharpRomans.Tests.Spec.Roman_Numeral
 			_operation = op.Execute(_subject, right);
 		}
 
-		private void IsGreaterThanZero()
+		private void isGreaterThanZero()
 		{
 			Assert.True(_comparison > 0);
 		}
 
-		private void IsZero()
+		private void isZero()
 		{
 			Assert.Equal(0, _comparison);
 		}
 
-		private void IsLessThanZero()
+		private void isLessThanZero()
 		{
 			Assert.True(_comparison < 0);
 		}
 
-		private void Is_(bool value)
+		private void @is(bool value)
 		{
 			Assert.Equal(value, _operation);
 		}
