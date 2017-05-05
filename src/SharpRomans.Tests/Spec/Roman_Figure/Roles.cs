bbc122ca0cc247136bcabd0ffd1798b22648f73a
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2017-05-05 10:28:14 +0200

    fixed feature RomanFigure.Roles

--- src/SharpRomans.Tests/Spec/Roman_Figure/Roles.cs
+++ src/SharpRomans.Tests/Spec/Roman_Figure/Roles.cs
@@ -4,7 +4,7 @@ using Xunit;
 
 namespace SharpRomans.Tests.Spec.Roman_Figure
 {
-	[Category("Spec"), Category("RomanFigure"), Category("Roles")]
+	[Category("Spec", Subject = "RomanFigure", Feature = "Roles")]
 	[Collection("bddfy")]
 	[Story(
 		Title = "roles",
@@ -12,21 +12,21 @@ namespace SharpRomans.Tests.Spec.Roman_Figure
 		IWant = "to be able to invoke a property on a roman figure",
 		SoThat = "I am able to know what the role of the figure is in a roman numeral"
 	)]
-	public class Roles
+	public class RolesTester
 	{
 		[Fact]
 		public void Repeatibility()
 		{
 			this.WithTags("RomanFigure", "Roles", "Repeatibility")
-				.Given(_ => _.aRepeteableRomanFigure_(RomanFigure.I))
+				.Given(_ => _.aRepeteableRomanFigure(RomanFigure.I))
 				.When(_ => _.repeteabilityIsChecked())
-				.Then(_ => _.repeteabilityIs_(true))
+				.Then(_ => _.repeteabilityIs(true))
 				.BDDfy("repeteable figure");
 
 			this.WithTags("RomanFigure", "Roles", "Repeatibility")
-				.Given(_ => _.aNonRepeteableRomanFigure_(RomanFigure.V))
+				.Given(_ => _.aNonRepeteableRomanFigure(RomanFigure.V))
 				.When(_ => _.repeteabilityIsChecked())
-				.Then(_ => _.repeteabilityIs_(false))
+				.Then(_ => _.repeteabilityIs(false))
 				.BDDfy("non-repeteable figure");
 		}
 
@@ -36,28 +36,28 @@ namespace SharpRomans.Tests.Spec.Roman_Figure
 			this.WithTags("RomanFigure", "Roles", "Substractiveness")
 				.Given(_ => _.aSubstractiveRomanFigure_(RomanFigure.I))
 				.When(_ => _.substractivenessIsChecked())
-				.Then(_ => _.substractivenessIs_(true))
+				.Then(_ => _.substractivenessIs(true))
 				.BDDfy("substractive figure");
 
 			this.WithTags("RomanFigure", "Roles", "Substractiveness")
-				.Given(_ => _.aNonSubstractiveRomanFigure_(RomanFigure.V))
+				.Given(_ => _.aNonSubstractiveRomanFigure(RomanFigure.V))
 				.When(_ => _.substractivenessIsChecked())
-				.Then(_ => _.substractivenessIs_(false))
+				.Then(_ => _.substractivenessIs(false))
 				.BDDfy("non-substractive figure");
 		}
 
 		private RomanFigure _subject;
-		private void aRepeteableRomanFigure_(RomanFigure figure)
+		private void aRepeteableRomanFigure(RomanFigure figure)
 		{
 			_subject = figure;
 		}
 		
-		private void aNonRepeteableRomanFigure_(RomanFigure figure)
+		private void aNonRepeteableRomanFigure(RomanFigure figure)
 		{
 			_subject = figure;
 		}
 
-		private void aNonSubstractiveRomanFigure_(RomanFigure figure)
+		private void aNonSubstractiveRomanFigure(RomanFigure figure)
 		{
 			_subject = figure;
 		}
@@ -68,7 +68,7 @@ namespace SharpRomans.Tests.Spec.Roman_Figure
 			_repeteability = _subject.IsRepeteable;
 		}
 		
-		private void repeteabilityIs_(bool repeteability)
+		private void repeteabilityIs(bool repeteability)
 		{
 			Assert.Equal(repeteability, _repeteability);
 		}
@@ -84,7 +84,7 @@ namespace SharpRomans.Tests.Spec.Roman_Figure
 			_substractiveness = _subject.IsSubstractive;
 		}
 		
-		private void substractivenessIs_(bool substractiveness)
+		private void substractivenessIs(bool substractiveness)
 		{
 			Assert.Equal(substractiveness, _substractiveness);
 		}
